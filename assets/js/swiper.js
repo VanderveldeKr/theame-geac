e1d34b90219046d36e12108db5ea283af4890bcd
Roman Detkin <vanderveldekr@gmail.com>
2023-10-30 00:14:58 +0500

    Сделан footer, начат слайдер, готова навигация

---
+++ assets/js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
