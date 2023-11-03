15206d1a491baecdcf1e53575a91cde5bd8e18dc
Roman Detkin <vanderveldekr@gmail.com>
2023-11-03 12:37:52 +0500

    + mobile version, правки по тексту

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,13 +1,37 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+    // Опциональные параметры
     slidesPerView: 3,
     spaceBetween: 30,
-    // Navigation arrows
+    // Навигационные стрелки
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-
-});
-
-
+  });
+    
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+    
+    button.on('click', (e) => {
+      e.preventDefault();
+      toggleMenu();
+    });
+    
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+    
+    function toggleMenu(){
+      menu.toggleClass('burger-menu_active');
+      
+      if (menu.hasClass('burger-menu_active')) {
+        $('body').css('overlow', 'hidden');
+      } else {
+        $('body').css('overlow', 'visible');
+      }
+    }
+  }
+  
+  burgerMenu('.burger-menu');
